f34750daf783593f164e7f5a4bfdafd38a821895
ccuellarp <camilocuellarp@hotmail.com>
2024-12-28 00:17:04 -0500

    Prueba 2 Despliegue

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+
 import Header from './components/Header'
 import Footer from './components/Footer'
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Header = () => {
     return (
         <header className="bg-primary text-white p-3 fixed-top w-100">
-            <div className="container">
+            <div className="container d-flex justify-content-between align-items-center">
                 <h1 className="display-4">App Relatos de papel</h1>
+                <div>
+                    <button className="btn btn-light me-2">
+                    </button>
+
+                    <button className="btn btn-light me-2">
+                    </button>
+                    
+                </div>
             </div>
         </header>
     );
